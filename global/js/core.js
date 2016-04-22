e293184d53111161f247dbda078491bfd1c55cc4
Nicolae Herrera <nherrera@brainshark.com>
2016-04-21 23:05:18 -0400

    Tons of updates
    cleaned up, adjusted to updated ui, and more.

--- global/js/core.js
+++ global/js/core.js
@@ -1,4 +1,8 @@
-
+/*!
+ * remark (http://getbootstrapadmin.com/remark)
+ * Copyright 2015 amazingsurge
+ * Licensed under the Themeforest Standard Licenses
+ */
 (function(window, document, $) {
   'use strict';
 
